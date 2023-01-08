7f148946ecaff5eb6dda3b40c6df04d3db2ff0a1
viniciussousav <vsv@cin.ufpe.br>
2023-01-08 20:46:53 -0300

    Add Migration Files

---
+++ src/Colabora.Infrastructure/Persistence/Migrations/20230108234300_VolunteerTableStructure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Colabora.Infrastructure.Persistence.Migrations
+{
+    public partial class VolunteerTableStructure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ORGANIZATION",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORGANIZATION", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VOLUNTEER",
+                columns: table => new
+                {
+                    VolunteerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Interests = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VOLUNTEER", x => x.VolunteerId);
+                    table.UniqueConstraint("AK_VOLUNTEER_Email", x => x.Email);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ORGANIZATION");
+
+            migrationBuilder.DropTable(
+                name: "VOLUNTEER");
+        }
+    }
+}
