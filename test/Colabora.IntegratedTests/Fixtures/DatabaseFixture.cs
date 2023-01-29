1b88c1fc59f0829c97bee8a97fabd8479ef87078
viniciussousav <vsv@cin.ufpe.br>
2023-01-29 10:28:02 -0300

    [test]: DatabaseFixture.cs refactoring

--- test/Colabora.IntegratedTests/Fixtures/DatabaseFixture.cs
+++ test/Colabora.IntegratedTests/Fixtures/DatabaseFixture.cs
@@ -3,24 +3,26 @@ using Colabora.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
+// ReSharper disable ClassNeverInstantiated.Global
+
 namespace Colabora.IntegrationTests.Fixtures;
 
 public class DatabaseFixture
 {
     private readonly AppDbContext _appDbContext;
+    private readonly IConfigurationRoot _configuration;
     public DatabaseFixture()
     {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Test.json")
+            .Build();
+        
         _appDbContext = CreateContext();
     }
     
-    private static IConfiguration GetConfiguration() =>
-        new ConfigurationBuilder()
-            .AddJsonFile("appsettings.Test.json")
-            .Build();
-    
-    private static AppDbContext CreateContext()
+    private AppDbContext CreateContext()
     {
-        var connectionString = GetConfiguration().GetConnectionString("ColaboraDatabase");
+        var connectionString = _configuration.GetConnectionString("ColaboraDatabase");
         return new AppDbContext(
             new DbContextOptionsBuilder<AppDbContext>()
                 .UseSqlServer(connectionString)
